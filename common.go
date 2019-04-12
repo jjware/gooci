085424c12c317027bfed51d0eb6c800d5556f780
Joshua Jones <jjones@criticalmix.com>
2019-04-12 15:22:48 -0400

    add ConnectionPoolCreate function

--- common.go
+++ common.go
@@ -7,7 +7,6 @@ import (
 	"errors"
 	"fmt"
 	"log"
-	"strconv"
 	"unsafe"
 )
 
@@ -25,7 +24,7 @@ const (
 	ModeNewLengthSemantics = Mode(C.OCI_NEW_LENGTH_SEMANTICS)
 )
 
-func firstNullByteIndex(s []C.uchar) int {
+func firstNullByteIndex(s []byte) int {
 	for i := 0; i < len(s); i++ {
 		if 0 == s[i] {
 			return i
@@ -34,10 +33,24 @@ func firstNullByteIndex(s []C.uchar) int {
 	return -1
 }
 
-type cString []C.uchar
+func cStringToGoString(s *C.uchar, l C.int) (result string) {
+	size := int(unsafe.Sizeof(*s))
+	length := int(l)
 
-func (m cString) String() string {
-	return fmt.Sprintf("%v", m[0:firstNullByteIndex(m)])
+	byt := C.GoBytes(unsafe.Pointer(s), (C.int)(size*length))
+
+	return string(byt[0:firstNullByteIndex(byt)])
+}
+
+func goStringToCString(s string) *C.uchar {
+	arr := make([]C.uchar, len(s)+1)
+	i := 0
+
+	for ; i < len(s); i++ {
+		arr[i] = C.uchar(s[i])
+	}
+	arr[i] = 0
+	return &arr[0]
 }
 
 type errorRecord map[int]string
@@ -46,12 +59,12 @@ func (e errorRecord) Error() string {
 	str := ""
 	ctr := 0
 
-	for k, v := range e {
+	for _, v := range e {
 
 		if ctr > 0 {
 			str = str + ": "
 		}
-		str = str + strconv.Itoa(k) + " - " + v
+		str += v
 		ctr++
 	}
 	return str
@@ -60,29 +73,26 @@ func (e errorRecord) Error() string {
 func getError(handlep unsafe.Pointer, handleType C.ub4) error {
 	var sqlState *C.OraText
 	var eCode C.sb4
-	eMessage := make(cString, maxErrorMessageSize)
+	eMessage := make([]C.uchar, maxErrorMessageSize)
 	eRecord := make(errorRecord)
 
-	for {
-		r2 := C.OCIErrorGet(
-			handlep,
-			C.ub4(1),
-			sqlState,
-			&eCode,
-			&eMessage[0],
-			C.uint(maxErrorMessageSize),
-			handleType,
-		)
+	r2 := C.OCIErrorGet(
+		handlep,
+		C.ub4(1),
+		sqlState,
+		&eCode,
+		&eMessage[0],
+		C.uint(maxErrorMessageSize),
+		handleType,
+	)
 
-		if C.OCI_ERROR == r2 {
-			return errors.New("message larger than buffer")
-		} else if C.OCI_INVALID_HANDLE == r2 {
-			return errors.New("invalid handle")
-		} else if C.OCI_SUCCESS == r2 {
-			eRecord[int(eCode)] = eMessage.String()
-		} else {
-			break
-		}
+	if C.OCI_ERROR == r2 {
+		return errors.New("message larger than buffer")
+	} else if C.OCI_INVALID_HANDLE == r2 {
+		return errors.New("invalid handle")
+	} else if C.OCI_SUCCESS == r2 {
+		fmt.Println("get error: success")
+		eRecord[int(eCode)] = cStringToGoString(&eMessage[0], maxErrorMessageSize)
 	}
 	return eRecord
 }
