ed813b1e2e7f91572e9baa601259584c6e59c1fd
Joshua Jones <joshua.jones.software@gmail.com>
2019-04-17 01:51:14 -0400

    add connection pool functions

---
+++ relational.go
@@ -0,0 +1,53 @@
+package gooci
+
+// #include "gooci.h"
+import "C"
+
+func ConnectionPoolCreate(
+	envp *Env,
+	errp *Error,
+	cpp *CPool,
+	namep *string,
+	connstr string,
+	min int,
+	max int,
+	incr int,
+	username string,
+	password string,
+	mode Mode,
+) Result {
+	var cstrName *C.uchar
+	var nameLen C.sb4
+
+	cstrConnstr := goStringToCString(connstr)
+	cstrUsername := goStringToCString(username)
+	cstrPassword := goStringToCString(password)
+
+	result := C.OCIConnectionPoolCreate(
+		(*C.OCIEnv)(envp),
+		(*C.OCIError)(errp),
+		(*C.OCICPool)(cpp),
+		&cstrName,
+		&nameLen,
+		cstrConnstr,
+		C.sb4(len(connstr)),
+		C.ub4(min),
+		C.ub4(max),
+		C.ub4(incr),
+		cstrUsername,
+		C.sb4(len(username)),
+		cstrPassword,
+		C.sb4(len(password)),
+		C.ub4(mode),
+	)
+	*namep = cStringToGoString(cstrName, int(nameLen))
+	return Result(result)
+}
+
+func ConnectionPoolDestroy(cpp *CPool, errp *Error) Result {
+	return Result(C.OCIConnectionPoolDestroy(
+		(*C.OCICPool)(cpp),
+		(*C.OCIError)(errp),
+		C.OCI_DEFAULT,
+	))
+}
